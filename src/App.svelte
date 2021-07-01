914c85ab08db07ae3d334d7bb7b97aabf5b65a27
Sjoerd Weerstra <sjoerd.weerstra@valtech.com>
2021-07-01 18:46:05 +0200

    Add root path to router

--- src/App.svelte
+++ src/App.svelte
@@ -4,6 +4,7 @@
 </script>
 
 <Router>
+	<Route path="/" component={ChecklistPage} />
 	<Route path="items" component={ChecklistPage} />
 	<Route path="items/:slug" component={ChecklistPage} />
 </Router>
