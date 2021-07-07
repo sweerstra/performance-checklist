f7a43446d15e9e123c1a5f313a56cbfb45c31b75
Michiel Koning <michiel.koning@valtech.com>
2021-07-07 16:44:49 +0200

    addede postcss

--- src/components/ChecklistItem.svelte
+++ src/components/ChecklistItem.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
     import { createEventDispatcher } from "svelte";
+    import PerformanceDetails from "./PerformanceDetails.svelte";
+
     import type { ChecklistItemModel } from "../interfaces/ChecklistItem";
 
     export let item: ChecklistItemModel;
@@ -14,22 +16,10 @@
 
 <li class="item" class:is-active={isActive} on:click={onSelect}>
   <div class="checkbox"></div>
-  <div class="text">
-    <h2>{item.title}</h2>
-    <p>{item.description}</p>
-    <div class="tags">
-      {#each item.disciplines as discipline}
-        <div class="tag">{discipline}</div>
-      {/each}
-    </div>
-  </div>
+  <PerformanceDetails item={item} />
 </li>
 
 <style>
-
-    h2 {
-      margin: 0 0 0.25em;
-    }
 
     .checkbox {
       width: 1.5em;
@@ -56,16 +46,4 @@
         background-color: rgba(138,180,248,.04);
     }
 
-    .tags {
-      display: flex;
-      gap: 0.5em;
-    }
-
-    .tag {
-      padding: 0.25em 1em;
-      font-size: 0.8em;
-      border: 1px solid #8ab4f8;
-      color: #8ab4f8;
-      border-radius: 1em;
-    }
 </style>
---
+++ src/components/PerformanceDetails.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+    import type { ChecklistItemModel } from "../interfaces/ChecklistItem";
+
+    export let item: ChecklistItemModel;
+</script>
+
+<div class="text">
+  <h2>{item.title}</h2>
+  <p>{item.description}</p>
+  <div class="tags">
+    {#each item.disciplines as discipline}
+      <div class="tag">{discipline}</div>
+    {/each}
+  </div>
+</div>
+
+
+<style>
+
+    h2 {
+      margin: 0 0 0.25em;
+    }
+
+    .tags {
+      display: flex;
+      gap: 0.5em;
+    }
+
+    .tag {
+      padding: 0.25em 1em;
+      font-size: 0.8em;
+      border: 1px solid #8ab4f8;
+      color: #8ab4f8;
+      border-radius: 1em;
+    }
+</style>
