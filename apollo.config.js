88e1c00c0635c561991f60ce39ec033bc3a5ccc3
Michiel Koning <michiel.koning@valtech.com>
2021-09-02 17:04:40 +0200

    update gitignore

--- apollo.config.js
+++ apollo.config.js
@@ -1,13 +1,12 @@
 module.exports = {
-    client: {
-        service: {
-            name: 'performancechecklist',
-            url: 'https://graphql.contentstack.com/stacks/bltee47bccbc9d4dfc7?environment=localhost',
-            headers: {
-                access_token: 'csdd84e745e89eeff37eb158c7'
-            },
-            skipSSLValidation: true
-        },
-        includes: ['**/*.graphql', 'src/data/*.ts'],
+  client: {
+    service: {
+      name: "performancechecklist",
+      url: "https://graphql.contentstack.com/stacks/bltee47bccbc9d4dfc7?environment=localhost",
+      headers: {
+        access_token: "csdd84e745e89eeff37eb158c7",
+      },
     },
+    includes: ["**/*.graphql", "src/data/*.ts"],
+  },
 };
